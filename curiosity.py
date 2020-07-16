5307adfdd774376c73bd9368140274fe15f49065
Kyosuke Tawara <astrowidow@gmail.com>
2020-07-16 22:33:14 +0900

    Functionに微分を算出するための仮想関数を追加した。

--- curiosity.py
+++ curiosity.py
@@ -5,10 +5,12 @@ import numpy as np
 class Variable:
     def __init__(self, data):
         self.data = data
+        self.grad = None
 
 
 class Function:
     def __call__(self, input_variable):
+        self.input_variable = input_variable
         x = input_variable.data
         y = self.forward(x)
         output_variable = Variable(y)
@@ -17,13 +19,28 @@ class Function:
     def forward(self, x):
         raise NotImplementedError()
 
+    def backward(self, dl_dy):
+        raise NotImplementedError()
+
 
 # Element functions
 class Squared(Function):
     def forward(self, x):
         return x**2
 
+    def backward(self, dl_dy):
+        x = self.input_variable.data
+        dy_dx = 2*x
+        dl_dx = dl_dy*dy_dx
+        return dl_dx
+
 
 class Exp(Function):
     def forward(self, x):
         return np.exp(x)
+
+    def backward(self, dl_dy):
+        x = self.input_variable.data
+        dy_dx = np.exp(x)
+        dl_dx = dl_dy*dy_dx
+        return dl_dx

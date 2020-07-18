62ad82ddf19a059e4b0b0d6c3f6cf1137afe7e30
Kyosuke Tawara <astrowidow@gmail.com>
2020-07-18 22:16:38 +0900

    Functionクラスのcall入力の可変長引数に対応した。

--- curiosity.py
+++ curiosity.py
@@ -15,6 +15,9 @@ class Variable:
     def set_creator(self, func):
         self.creator = func
 
+    def clear_grad(self):
+        self.grad = None
+
     def back_prop(self):
         if self.grad is None:
             self.grad = np.ones_like(self.data)
@@ -22,13 +25,23 @@ class Variable:
         f = self.creator
         if f is not None:
             # y = f(x)
-            # extracts the input variable 'x'
-            variable_x = f.input_variable
+            # extracts the input variables 'x'
+            x_tuple = f.input_variables
             # dL/dx = dy/dx * dL/dy
             # dy/dx: df/dx
-            # dL/dy: self.grad
-            variable_x.grad = f.backward(self.grad)
-            variable_x.back_prop()
+            # dL/dy: self.grad (It is assumed that there is only one output of 'f')
+            x_grad_tuple = f.backward(self.grad)
+            if not isinstance(x_grad_tuple, tuple):
+                x_grad_tuple = (x_grad_tuple,)
+            for x_variable, x_grad in zip(x_tuple, x_grad_tuple):
+                if x_variable.grad is None:
+                    x_variable.grad = x_grad
+                else:
+                    # An implementation of 'x_variable.grad += x_grad' cannot be used.
+                    # Because 'x_grad' has type of 'ndarray',
+                    # 'x_grad' may be REFERENCE to 'y_grad'.
+                    x_variable.grad = x_variable.grad + x_grad
+                x_variable.back_prop()
 
 
 def as_array(x):
@@ -38,11 +51,14 @@ def as_array(x):
 
 
 class Function:
-    def __call__(self, input_variable):
-        self.input_variable = input_variable
-        x = input_variable.data
+    def __call__(self, *input_variables):
+        # allows multiple arguments
+        self.input_variables = input_variables
+        x_tuple = (input_val.data for input_val in input_variables)
         # y = f(x)
-        y = self.forward(x)
+        # 'x' is 'tuple' of 'ndarray' because of the case that there are multiple inputs
+        # 'y' is assumed that its type is 'ndarray'
+        y = self.forward(*x_tuple)
         # There is the case that 'y' is returned as scalar
         # 'as_array' turns type 'scalar' into type 'ndarray'
         output_variable = Variable(as_array(y))
@@ -58,13 +74,29 @@ class Function:
         raise NotImplementedError()
 
 
+# Operator functions
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+
+    def backward(self, dl_dy):
+        dl_dx0 = dl_dy
+        dl_dx1 = dl_dy
+        return dl_dx0, dl_dx1
+
+
+def add(x0, x1):
+    return Add()(x0, x1)
+
+
 # Element functions
 class Square(Function):
     def forward(self, x):
         return x**2
 
     def backward(self, dl_dy):
-        x = self.input_variable.data
+        x = self.input_variables[0].data
         dy_dx = 2*x
         dl_dx = dl_dy*dy_dx
         return dl_dx
@@ -79,7 +111,7 @@ class Exp(Function):
         return np.exp(x)
 
     def backward(self, dl_dy):
-        x = self.input_variable.data
+        x = self.input_variables[0].data
         dy_dx = np.exp(x)
         dl_dx = dl_dy*dy_dx
         return dl_dx

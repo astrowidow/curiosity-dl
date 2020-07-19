91e8c318bb39a5da5a5f5205f87a57932765048f
Kyosuke Tawara <astrowidow@gmail.com>
2020-07-20 03:08:00 +0900

    Variableクラスの基礎的な演算子をオーバロードし、Intやndarrayとの演算を行えるようにした。

--- curiosity.py
+++ curiosity.py
@@ -24,12 +24,15 @@ def no_grad():
 
 # Base classes
 class Variable:
-    def __init__(self, data):
+    __array_priority__ = 200
+
+    def __init__(self, data, name=None):
         if data is not None:
             if not isinstance(data, np.ndarray):
                 raise TypeError('{} is not supported'.format(type(data)))
 
         self.data = data
+        self.name = name
         self.grad = None
         self.creator = None
         self.generation = 0
@@ -86,6 +89,35 @@ class Variable:
             if not retain_grad:
                 y_variable.grad = None
 
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        if self.data is None:
+            return 'variable(None)'
+        p = str(self.data).replace('\n', '\n' + ' ' * 9)
+        return 'variable(' + p + ')'
+
+    @property
+    def shape(self):
+        return self.data.shape
+
+    @property
+    def ndim(self):
+        return self.data.ndim
+
+    @property
+    def size(self):
+        return self.data.size
+
+    @property
+    def dtype(self):
+        return self.data.dtype
+
+    @property
+    def T(self):
+        return Variable(self.data.T)
+
 
 def as_array(x):
     if np.isscalar(x):
@@ -93,8 +125,15 @@ def as_array(x):
     return x
 
 
+def as_variable(obj):
+    if isinstance(obj, Variable):
+        return obj
+    return Variable(obj)
+
+
 class Function:
     def __call__(self, *input_variables):
+        input_variables = [as_variable(input_val) for input_val in input_variables]
         x_tuple = (input_val.data for input_val in input_variables)
         # y = f(x)
         # 'x' is 'tuple' of 'ndarray' because of the case that there are multiple inputs
@@ -126,6 +165,18 @@ class Function:
 
 
 # Operator functions
+class Neg(Function):
+    def forward(self, x):
+        return -x
+
+    def backward(self, dl_dy):
+        return -dl_dy
+
+
+def neg(x):
+    return Neg()(x)
+
+
 class Add(Function):
     def forward(self, x0, x1):
         y = x0 + x1
@@ -138,7 +189,99 @@ class Add(Function):
 
 
 def add(x0, x1):
+    x1 = as_array(x1)
     return Add()(x0, x1)
+
+
+class Sub(Function):
+    def forward(self, x0, x1):
+        y = x0 - x1
+        return y
+
+    def backward(self, dl_dy):
+        dl_dx0 = dl_dy
+        dl_dx1 = -dl_dy
+        return dl_dx0, dl_dx1
+
+
+def sub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x0, x1)
+
+
+def rsub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x1, x0)
+
+
+class Mul(Function):
+    def forward(self, x0, x1):
+        y = x0 * x1
+        return y
+
+    def backward(self, dl_dy):
+        dl_dx0 = dl_dy*self.input_variables[1].data
+        dl_dx1 = dl_dy*self.input_variables[0].data
+        return dl_dx0, dl_dx1
+
+
+def mul(x0, x1):
+    x1 = as_array(x1)
+    return Mul()(x0, x1)
+
+
+class Div(Function):
+    def forward(self, x0, x1):
+        y = x0/x1
+        return y
+
+    def backward(self, dl_dy):
+        x0 = self.input_variables[0].data
+        x1 = self.input_variables[1].data
+        dl_dx0 = dl_dy / x1
+        dl_dx1 = dl_dy * (-x0/x1**2)
+        return dl_dx0, dl_dx1
+
+
+def div(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x0, x1)
+
+
+def rdiv(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x1, x0)
+
+
+class Pow(Function):
+    def __init__(self, c):
+        self.c = c
+
+    def forward(self, x):
+        y = x ** self.c
+        return y
+
+    def backward(self, dl_dy):
+        x = self.input_variables[0].data
+        c = self.c
+        dl_dx = c*x**(c - 1)*dl_dy
+        return dl_dx
+
+
+def pow(x, c):
+    return Pow(c)(x)
+
+
+Variable.__neg__ = neg
+Variable.__add__ = add
+Variable.__radd__ = add
+Variable.__sub__ = sub
+Variable.__rsub__ = rsub
+Variable.__mul__ = mul
+Variable.__rmul__ = mul
+Variable.__truediv__ = div
+Variable.__rtruediv__ = rdiv
+Variable.__pow__ = pow
 
 
 # Element functions
